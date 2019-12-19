4d6884f50d4f66ea36fde6ab90bf2134653ecf15
Jose Daniel Carranza Leguizamo <josedaniel.carranza@globant.com>
2019-12-19 12:22:53 -0500

    unittest with resolve

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import HomePageView
 
 # Create your tests here.
 class HomePageTest(SimpleTestCase):
@@ -19,3 +20,10 @@ class HomePageTest(SimpleTestCase):
     
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, "hello world")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
