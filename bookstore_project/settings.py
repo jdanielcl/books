0f937d9b84a8e0e86086fb9f4b61a182c915beca
Jose Daniel Carranza Leguizamo <josedaniel.carranza@globant.com>
2019-12-20 11:05:54 -0500

    global variables were added

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8r37y(r9a$gkz$j5p7z3zq98+6&#j^g5+&uwlg7i^!ok7v+d*6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
